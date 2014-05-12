08362a2bc0086523219146285a463c91a1deadbf
hutiaxi <547897116@qq.com>
2014-05-12 11:30:27 +0800

    自制插件

---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+D:\testplugin\platforms\android\bin\classes.dex : \
+D:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\tx\testplugin\testplugin.class \
+D:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\tx\testplugin\BuildConfig.class \
+D:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\tx\testplugin\R$attr.class \
+D:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\tx\testplugin\R$drawable.class \
+D:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\tx\testplugin\R$string.class \
+D:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\tx\testplugin\R$xml.class \
+D:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\tx\testplugin\R.class \
+D:\testplugin\platforms\android\bin\dexedLibs\classes-9cc2014872d940f34526406f4c6d8848.jar \
